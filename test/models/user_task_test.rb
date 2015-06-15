c9a38a3795180fc3d61d43e997cb70c94e5f78a7
Sia <siakaramalegos@gmail.com>
2015-06-15 09:12:42 -0500

    Added validation testing for user tasks

--- test/models/user_task_test.rb
+++ test/models/user_task_test.rb
@@ -4,4 +4,16 @@ class UserTaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save task without description" do
+    user_task = UserTask.new
+    user_task.due = Date.new(2015, 10, 31)
+    assert_not user_task.save, "Saved the task without a description"
+  end
+
+  test "should not save a task without a due date" do
+    user_task = UserTask.new
+    user_task.description = "Make groceries"
+    assert_not user_task.save, "Saved the task without a due date"
+  end
 end
