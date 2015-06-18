390ff34e2b58f6ab8bb6d894d9668387ba9ec5f3
Sia <siakaramalegos@gmail.com>
2015-06-18 08:53:26 -0500

    Added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/about'
+
   resources :user_tasks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
