b708f6c787d5997f81ddacc46a334cdab2510db5
Sia <siakaramalegos@gmail.com>
2015-06-18 10:21:32 -0500

    Finished adding omniauth for facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   get 'pages/index'
-
   get 'pages/about'
+
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signout' => 'sessions#destroy', :as => :signout
 
   resources :user_tasks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'user_tasks#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
