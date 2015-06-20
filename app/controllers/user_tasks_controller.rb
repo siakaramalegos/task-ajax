03f2e2fec774af3a81183e23e6a81332e47000bf
Sia <siakaramalegos@gmail.com>
2015-06-19 21:32:39 -0500

    Restricted access to user's tasks only not all tasks

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,6 +1,7 @@
 class UserTasksController < ApplicationController
   before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_task
 
   # GET /user_tasks/new
   def new
@@ -11,6 +12,7 @@ class UserTasksController < ApplicationController
   # POST /user_tasks.json
   def create
     @user_task = UserTask.new(user_task_params)
+    @user_task.user_id = current_user.id
 
     respond_to do |format|
       if @user_task.save
@@ -53,17 +55,27 @@ class UserTasksController < ApplicationController
 
   private
     def all_tasks
-      @user_tasks = UserTask.order('due')
+      @user_tasks = UserTask.where(user_id: current_user.id).order('due')
       # @user_tasks_by_date = UserTask.group('due')
     end
 
     # Use callbacks to share common setup or constraints between actions.
     def set_user_task
       @user_task = UserTask.find(params[:id])
+      if current_user.id == @user_task.user_id
+        @user_task
+      else
+        invalid_task
+      end
+    end
+
+    def invalid_task
+      logger.error "Attempt to access invalid task #{params[:id]}"
+      redirect_to user_tasks_path, notice: 'Invalid task'
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_task_params
-      params.require(:user_task).permit(:description, :due)
+      params.require(:user_task).permit(:description, :due, :user_id)
     end
 end
